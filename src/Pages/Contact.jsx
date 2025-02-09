6a27b1ac89df1a75a4b94d2931f95db5364c005f
Devilwife <nicole.barger22@gmail.com>
2025-02-09 15:24:02 -0500

    updating the error.jsx coding

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import Form from "../components/Form";
+// import React from "react";
+// import Form from "../components/Form";
 
-const Contact = () => {
-  return (
-    <div>
-      <Form />
-    </div>
-  );
-};
+// const Contact = () => {
+//   return (
+//     <div>
+//       <Form />
+//     </div>
+//   );
+// };
 
-export default Contact;
+// export default Contact;
